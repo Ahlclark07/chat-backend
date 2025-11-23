e487bb29b13d3acf255672d2f804620d698782af
AhlClark07 <ahmalbilal@gmail.com>
2025-11-23 02:48:25 +0100

    update cors

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const corsOptions = {
   ],
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use((req, res, next) => {
   if (req.method === "OPTIONS") {
     return res.sendStatus(204);
@@ -35,7 +35,20 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use(
+  "/uploads",
+  cors(corsOptions),
+  express.static(path.join(__dirname, "..", "uploads"), {
+    setHeaders: (res) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+      );
+      res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS");
+    },
+  })
+);
 
 // =========================
 // Routes
