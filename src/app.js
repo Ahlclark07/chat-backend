dc6b6c9d59e9f2975d2bfe7db8492cdcc21f69e3
AhlClark07 <ahmalbilal@gmail.com>
2025-12-17 13:01:57 +0100

    eeki

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 });
 
 // =========================
-// Middlewares globaux
+// Middlewares globaux v
 // =========================
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
